1b9e2365f9d8669bc39efa5596737ab46a4eb29e
SamLavazais <samuel.lavazais@hotmail.fr>
2024-04-23 11:46:38 +0200

    display posts

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -13,7 +13,7 @@ class PostController extends Controller
      */
     public function index():View
     {
-        return view('posts.index');
+        return view('posts.index', ['posts' => Post::all()]);
     }
 
     /**
